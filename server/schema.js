b2410240d1b2d3b55e491a68794cba7d54b804d8
Manisha11sep <er.manisha11sep@gmail.com>
2018-05-17 00:03:27 -0700

    Author query

--- server/schema.js
+++ server/schema.js
@@ -1,13 +1,16 @@
 const graphql = require('graphql');
 
-const { GraphQLObjectType, GraphQLString, GraphQLSchema,GraphQLID,GraphQLInt } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLSchema,GraphQLID,GraphQLInt, GraphQLList } = graphql;
 
 
-var books = [
-    { name: 'Name of the Wind', genre: 'Fantasy', id: '1', authorId:'1'},
-    { name: 'The Final Empire', genre: 'Fantasy', id: '2',authorId:'2' },
-    { name: 'The Long Earth', genre: 'Sci-Fi', id: '3',authorId:'3' },
-];
+const books = [
+    {name: 'Name of the Wind', genre: 'Fantasy', id: '1', authorId: '1'},
+    {name: 'The Final Empire', genre: 'Fantasy', id: '2', authorId: '2'},
+    {name: 'The Long Earth', genre: 'Sci-Fi', id: '3', authorId: '3'},
+    {name: 'The Hero of Ages', genre: 'Fantasy', id: '4', authorId: '2'},
+    {name: 'The Colour of Magic', genre: 'Fantasy', id: '5', authorId: '3'},
+    {name: 'The Light Fantastic', genre: 'Fantasy', id: '6', authorId: '3'}
+  ];
 var authors = [
     { name: 'Patrick Rothfuss', age: 44, id: '1' },
     { name: 'Brandon Sanderson', age: 42, id: '2' },
@@ -41,7 +44,19 @@ const AuthorType = new GraphQLObjectType({
      
         name: { type: GraphQLString },
         age: { type: GraphQLInt },
-        id: { type: GraphQLID }
+        id: { type: GraphQLID },
+        books:{
+            type: new GraphQLList(BookType),
+            resolve(parent,args){
+                console.log("Parent id is ",parent.id)
+                return books.filter(book =>{
+                    return book.authorId===parent.id
+                }
+
+            )
+
+            }
+        }
     })
 });
 
